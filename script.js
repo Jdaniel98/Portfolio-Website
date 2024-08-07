9dabef0cdf6c0c43fb5b5a18190204d2d841360b
Daniel Owusu Mensah <danielowusumensah1998@gmail.com>
2024-08-07 23:13:39 +0000

    dark mode integrated

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+function toggleDarkMode() {
+    const body = document.body;
+    const darkModeIcon = document.getElementById("darkModeIcon");
+    
+    body.classList.toggle("dark-mode");
+    
+    if (body.classList.contains("dark-mode")) {
+      darkModeIcon.src = "assets/vector 2.jpg";
+      darkModeIcon.alt = "Light Mode";
+      localStorage.setItem("darkMode", "enabled");
+    } else {
+      darkModeIcon.src = "./assets/moon.png";
+      darkModeIcon.alt = "Dark Mode";
+      localStorage.setItem("darkMode", null);
+    }
+  }
+  
+  // Check for saved user preference, if any, on load of the website
+  document.addEventListener("DOMContentLoaded", function() {
+    const darkModeIcon = document.getElementById("darkModeIcon");
+    
+    if (localStorage.getItem("darkMode") === "enabled") {
+      document.body.classList.add("dark-mode");
+      darkModeIcon.src = "assets/vector 2.jpg";
+      darkModeIcon.alt = "Light Mode";
+    }
+  });  
